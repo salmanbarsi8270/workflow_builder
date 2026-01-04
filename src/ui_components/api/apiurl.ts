a8b6e0f6cba116660f7cf3fdad6cbd6e76d4ae6e
salman barsi <c.salman@faaztechsolutions.com>
2026-01-04 10:04:38 +0530

    UI runsidebar and condition cokor changes icons 9

--- src/ui_components/api/apiurl.ts
+++ src/ui_components/api/apiurl.ts
@@ -1,1 +1,2 @@
-export const API_URL = "https://workflow-builder-backend-6w5p.onrender.com";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+// export const API_URL = "https://workflow-builder-backend-6w5p.onrender.com";
