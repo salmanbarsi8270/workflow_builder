816f0027649da6199ce808821d1b57ea30651e5c
thanveer <thanveer21cs52@gmail.com>
2026-01-23 12:18:53 +0530

    added the api

--- src/ui_components/api/apiurl.ts
+++ src/ui_components/api/apiurl.ts
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-export const AI_URL = import.meta.env.VOLTAPP_URL || "http://localhost:4000";
+export const AI_URL = import.meta.env.VITE_VOLTAPP_URL || "http://localhost:4000";
 // export const API_URL = "https://workflow-builder-backend-6w5p.onrender.com";
